ee74594d127f439acbdafdce0297aa4576dbc27c
test <test@icomp.ufam.edu.br>
2018-04-10 13:00:54 -0400

    v1

---
+++ MicaelYanLab1Android/app/src/main/java/icomp/ufam/micaelyanlab1android/dao/DAOCountry.java
@@ -0,0 +1,40 @@
+package icomp.ufam.micaelyanlab1android;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Table(name="countries")
+public class DAOCountry extends Model {
+
+    @Column(name="name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public DAOCountry() {
+        super();
+    }
+
+    public static List<Country> getCountries() {
+        return new Select().from(DAOCountry.class).execute();
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+

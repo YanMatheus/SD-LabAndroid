d1f8b48df4b6eea5e0d4a04989d964d84df62d7b
Yan Matheus <ymgf@icomp.ufam.edu.br>
2018-04-12 02:13:31 -0400

    add umas viadage de layout

--- MicaelYanLab1Android/app/src/main/java/icomp/ufam/micaelyanlab1android/view/MainActivity.java
+++ MicaelYanLab1Android/app/src/main/java/icomp/ufam/micaelyanlab1android/view/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.webkit.WebView;
@@ -13,7 +14,9 @@ import icomp.ufam.micaelyanlab1android.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private WebView webView;
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -21,8 +24,10 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+
                     return true;
                 case R.id.navigation_dashboard:
+
                     Intent intent = new Intent(getBaseContext(), MapsActivity.class);
                     startActivity(intent);
                     return true;
@@ -35,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ActionBar actionBar = getSupportActionBar();
         webView = findViewById(R.id.webviewgit);
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
--- MicaelYanLab1Android/app/src/main/java/icomp/ufam/micaelyanlab1android/view/MapsActivity.java
+++ MicaelYanLab1Android/app/src/main/java/icomp/ufam/micaelyanlab1android/view/MapsActivity.java
@@ -1,7 +1,10 @@
 package icomp.ufam.micaelyanlab1android.view;
 
+import android.app.ActionBar;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,11 +13,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 import icomp.ufam.micaelyanlab1android.R;
+import icomp.ufam.micaelyanlab1android.controller.api.APICountries;
+import icomp.ufam.micaelyanlab1android.model.bean.Country;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private GoogleMap mMap ;
+    private Button voltar, mudarpais;
 
 
     @Override
@@ -25,14 +36,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        ActionBar actionBar = getActionBar();
+        voltar= findViewById(R.id.voltarButton);
+        mudarpais=findViewById(R.id.outropaisButton);
+
+
+
+        voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        mudarpais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData(v);
+            }
+        });
+    }
+
+    public void getData(View view){
+        APICountries.getRestCountriesClient().getCountries().enqueue(new Callback<List<Country>>() {
+            @Override
+            public void onResponse(Call<List<Country>> call, Response<List<Country>> response) {
+              if(response.isSuccessful()){  List<Country> countryList = response.body();
+                countryList.clear();
+                for (Country country : countryList){
+                    countryList.add(country);
+                }
+
+                }else{ System.out.println(response.errorBody());}
+            }
+
+            @Override
+            public void onFailure(Call<List<Country>> call, Throwable t) {
+                    t.printStackTrace();
+            }
+        });
 
     }
+
+
 
 
 }
